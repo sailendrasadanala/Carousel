cc3ece4518503d982d437cc7cebd67329c7e8e74
sailendra <sadanalasailendra@gmail.com>
2022-11-22 10:56:49 +0530

    last commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
-
+import "react-responsive-carousel/lib/styles/carousel.min.css"; 
+import { Carousel } from 'react-responsive-carousel';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Carousel>
+                
+                <div className='container'>
+                    <img src="assets/1.jpeg" className='image' />
+                    <p className="legend" style={{width:'40%',marginLeft:'-20%'}}>Wonder 1</p>
+                </div>
+                <div className='container'>
+                    <img src="assets/2.jpeg" />
+                    <p className="legend" style={{width:'40%',marginLeft:'-20%'}}>Wonder 2</p>
+                </div>
+                <div className='container'>
+                    <img src="assets/3.jpeg" />
+                    <p className="legend" style={{width:'40%',marginLeft:'-20%'}}>Wonder 3</p>
+                </div>
+                <div className='container'>
+                    <img src="assets/4.jpeg" />
+                    <p className="legend" style={{width:'40%',marginLeft:'-20%'}}>Wonder 4</p>
+                </div>
+                <div className='container'>
+                    <img src="assets/5.jpg" />
+                    <p className="legend" style={{width:'40%',marginLeft:'-20%'}}>Wonder 5</p>
+                </div>
+                <div className='container'>
+                    <img src="assets/6.jpeg" />
+                    <p className="legend" style={{width:'40%',marginLeft:'-20%'}}>Wonder 6</p>
+                </div>
+                <div className='container'>
+                    <img src="assets/7.jpg" />
+                    <p className="legend" style={{width:'40%',marginLeft:'-20%'}}>Wonder 7</p>
+                </div>
+            </Carousel>
+            
+
   );
 }
 
